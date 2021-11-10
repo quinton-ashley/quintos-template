a6d4fd027ef8db2137a784c05d47c906be97d78d
Quinton Ashley <qashto@gmail.com>
2021-11-10 14:12:29 -0500

    1.1.1

--- load.js
+++ load.js
@@ -1,4 +1,3 @@
 QuintOS.username = 'your-username'; // put your github username!
-QuintOS.level = 1; // the type of computer to use
 QuintOS.dir = '.'; // the folder with the game folder in it
-QuintOS.gameSelect = 'HelloWorld'; // load test program
+QuintOS.gameTitle = 'HelloWorld'; // the title of the game to load
