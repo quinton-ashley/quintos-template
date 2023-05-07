44b6b77c3981aa69666838a42341362e7bf0cfa9
Quinton Ashley <qashto@gmail.com>
2023-05-07 18:08:03 -0500

    1.2.2

--- load.js
+++ load.js
@@ -1,3 +1,2 @@
 QuintOS.user = 'your-username'; // put your github username!
-QuintOS.dir = 'GAMES'; // the folder with the game folder in it
 QuintOS.game = 'GuessTheNumber'; // the title of the game to load
